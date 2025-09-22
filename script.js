76594489cf938d517dbd19f30b253971539f0b56
Novikoff1997 <andreytoys@yandex.ru>
2025-09-23 01:49:03 +0300

    Верстка завершена, начало js

---
+++ script.js
@@ -0,0 +1,12 @@
+const cardsWrapper = document.querySelector(".content__cards");
+
+const films = [
+  {
+    id: 0,
+    title: "Марсианин,",
+    original: "The Martian 2015",
+    category: "",
+    rating: "7.8",
+    image: "./images/film.jpg",
+  },
+];
